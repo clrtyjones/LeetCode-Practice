eb84c789bce905f51289bde6d487cdec14a722b0
Jonathan Zepeda <jdm.johnz@gmail.com>
2024-02-02 00:04:49 -0800

    1952 Three Divisors Python Solution

--- Easy/1952_Three-Divisors/sol.py
+++ Easy/1952_Three-Divisors/sol.py
@@ -1,3 +1,12 @@
 class Solution:
     def isThree(self, n: int) -> bool:
+        divisors = 0
+
+        for i in range(n, 0, -1):
+            if n % i == 0:
+                divisors += 1
         
+        if divisors == 3:
+            return True
+
+        return False
